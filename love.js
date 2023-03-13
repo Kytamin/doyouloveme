40acf6662469f29f233638e50e028ed492ec8f93
KY <nguyenhoangky23121997@gmail.com>
2023-03-13 17:16:57 +0700

    00

---
+++ love.js
@@ -0,0 +1,10 @@
+function sayyes(){
+    alert("I love u too <3")
+}
+function neverthis(){
+    let a= Math.floor(Math.random()*window.innerWidth);
+    let b= Math.floor(Math.random()*window.innerHeight);
+        document.getElementById("no").style.left=a+"px"
+        document.getElementById("no").style.top=b+"px"
+}
+
